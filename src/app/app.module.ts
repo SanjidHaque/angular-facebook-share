8deaab0e4bd718142ef52444a6d47efe553f7ec9
Sanjidul <Sanjidul>
2025-02-18 02:09:53 +0600

    Add angular ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +18,9 @@ import {ShareButtonDirective} from 'ngx-sharebuttons';
     ShareButtonDirective,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
